78d03cdce0196523a9b003f876985e8aadf5ae80
groundsmind <groundsmind@gmail.com>
2026-05-05 20:37:57 -0300

    primeira iteração do trabalho

---
+++ ALG/Trabalhos/T1/main.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+Tabela de filmes e séries:
+
+|------------------------------------|------|---------|-----------|-----------|
+|               Título               | Tipo | Censura | Categoria | Streaming |
+|------------------------------------|------|---------|-----------|-----------|
+|"Everything Everywhere All At Once" |  01  |   +18   |     1     |     2     |
+|"Good Omens"                        |  02  |   +10   |     3     |     1     |
+|"Whiplash"                          |  01  |   +18   |     2     |     1     |
+|"Friends"                           |  02  |   +10   |     3     |     1     |
+|"NOPE"                              |  01  |   +18   |     4     |     2     |
+|"Scott Pilgrim Vs. The World"       |  01  |   +18   |     3     |     1     |
+|"Loki"                              |  02  |   +10   |     2     |     3     |
+|"Hereditary"                        |  01  |   +18   |     4     |     2     |
+|"Smiling Friends"                   |  02  |   +18   |     3     |     4     |
+|"A Minecraft Movie"                 |  01  |    0    |     3     |     2     |
+|------------------------------------|------|---------|-----------|-----------|
+
+Categoria:
+1 - Ação
+2 - Drama
+3 - Comédia
+4 - Terror
+
+Streaming:
+1 - Netflix
+2 - Prime Video
+3 - Disney+
+4 - HBO Max
+
+*/
+
+void main()
+{
+    int escolha_usuario = escolher_filtros();
+
+    filtrar_midia(escolha_usuario);
+}
+
+int escolher_filtros()
+{
+    int escolha, temp;
+    
+    printf("tipo");
+    scanf("%i", &temp);
+    if ((temp != 1) || (temp != 2))
+    {
+        printf("Tipo deve ser 1 ou 2!");
+        escolha = escolher_filtros();
+        return escolha;
+    }
+    escolha += temp * 10000;
+
+
+    printf("censura");
+    scanf("%i", &temp);
+    if ((temp != 0) || (temp != 10) || (temp != 18))
+    {
+        printf("Censura deve ser 0, 10 ou 18!");
+        escolha = escolher_filtros();
+        return escolha;
+    }
+    escolha += temp * 100;
+
+
+    printf("categoria");
+    scanf("%i", &temp);
+    if ((temp >= 1) || (temp <= 4))
+    {
+        printf("Categoria deve ser de 1 a 4!");
+        escolha = escolher_filtros();
+        return escolha;
+    }
+    escolha += temp * 10;
+
+
+    printf("streaming");
+    scanf("%i", &temp);
+    if ((temp >= 1) || (temp <= 4))
+    {
+        printf("Streaming deve ser de 1 a 4!");
+        escolha = escolher_filtros();
+        return escolha;
+    }
+    escolha += temp;
+
+
+
+    return escolha;
+}
+
+void filtrar_midia(int filtro)
+{
+    printf("\n\nMídia recomendada:\n");
+    if (filtro == 11812)
+    {
+        printf("Everything Everywhere All At Once");
+    }
+    else if (filtro == 21031)
+    {
+        printf("Good Omens");
+    }
+    else if (filtro == 11821)
+    {
+        printf("Whiplash");
+    }
+    else if (filtro == 21031)
+    {
+        printf("Friends");
+    }
+    else if (filtro == 11842)
+    {
+        printf("NOPE");
+    }
+    else if (filtro == 11831)
+    {
+        printf("Scott Pilgrim Vs. The World");
+    }
+    else if (filtro == 21023)
+    {
+        printf("Loki");
+    }
+    else if (filtro == 11842)
+    {
+        printf("Hereditary");
+    }
+    else if (filtro == 21834)
+    {
+        printf("Smiling Friends");
+    }
+    else if (filtro == 10032)
+    {
+        printf("A Minecraft Movie");
+    }
+    else
+    {
+        printf("\n\nNenhum filme/série encontrado a partir desse filtro.");
+    }
+    printf("\n");
+}
